29a8fe5c7b05b0c25f24d02e1372edfead70ff0b
Gamer122333HUN <cz.gergo12@gmail.com>
2025-07-08 22:07:26 +0200

    mentés

--- main.py
+++ main.py
@@ -16,27 +16,45 @@ def createGraph():
         fid = feature.id()
         spatial_index.addFeature(feature)
         graph.add_node(fid, pos=centroid)
-    distances = []
+
     # Élek hozzáadása
     for i in range(len(features) - 1):
         for k in range(i + 1, len(features)):
             geom1 = features[i].geometry()
             geom2 = features[k].geometry()
             shortest_line = geom1.shortestLine(geom2)
-            distance = shortest_line.length() / feet
+            distance = shortest_line.length() * 100#feet
             distances.append(distance)
-            if distance < max_distance:
+            if distance < max_distance_for_graph:
                 graph.add_edge(features[i].id(), features[k].id(), weight=distance)
 
 def stats():
+    print(f"CRS: {layer.crs()}")
     print(f"Csúcsok száma: {graph.number_of_nodes()}")
     print(f"Élek száma: {graph.number_of_edges()}")
     print(f"max táv: {max(distances)}")
     print(f"min táv: {min(distances)}")
     print(f"átl táv: {sum(distances) / len(distances)}")
 
+def showGraph():
+    pos = nx.get_node_attributes(graph, 'pos')
+    nx.draw(graph, pos, with_labels=False, node_size=20)
+    rounded_labels = {
+        (u, v): f"{d['weight']:.2f}"
+        for u, v, d in graph.edges(data=True)
+    }
+    labels = nx.get_edge_attributes(graph, 'weight')
+    nx.draw_networkx_edge_labels(graph, pos, edge_labels=rounded_labels)
+    plt.show()
+
+def createLayer():
+    name = f"new_{layer.name()}"
+    crs = layer.crs()
+    geometry = layer.geometry()
+
 # Beállítások
-max_distance = 10000  # Maximális éltávolság méterben
+max_distance_for_graph = 100000  # Maximális éltávolság méterben a gráfhoz adáshoz
+max_distance_for_merge = 10000  # Maximális éltávolság méterben az összeolvasztáshoz
 feet = 0.3048   # láb váltószáma
 
 # Réteg betöltése
@@ -46,26 +64,20 @@ features = list(layer.getFeatures())
 # Gráf és térbeli index inicializálása
 graph = nx.Graph()
 spatial_index = QgsSpatialIndex()
+distances = []
 
 createGraph()
 
+
+
+
 stats()
+showGraph()
 
 
 
 
 
 
-# Gráf használata
 
 
-
-
-# Grafikon megjelenítése (opcionális)
-
-
-pos = nx.get_node_attributes(graph, 'pos')
-nx.draw(graph, pos, with_labels=False, node_size=20)
-labels = nx.get_edge_attributes(graph, 'weight')
-nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels)
-plt.show()
