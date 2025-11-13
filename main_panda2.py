21436a8c698e616c25da862d849d932e697ad026
Gamer122333HUN <cz.gergo12@gmail.com>
2025-11-13 09:39:59 +0100

    multipolygonos kirajzolás

--- main_panda2.py
+++ main_panda2.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import plotly.graph_objs as go
 import plotly.express as px
 import numpy as np
-
+from shapely import MultiPolygon
+import time
 
 
 def process_shapefile(shapefile_path):
@@ -55,16 +56,30 @@ def showGraph_plotter():
 
 def showGraph_plotly():
     fig = go.Figure()
-
+    polygondb = 0
     for _, row in gdf.iterrows():
-        x, y = np.array(row.geometry.exterior.xy)
-        fig.add_trace(go.Scattermap(
-            lon=x,
-            lat=y,
-            mode='lines',
-            line=dict(width=2, color='blue'),
-            name=str(row.get('name', 'Polygon'))
-        ))
+        if type(row.geometry) == MultiPolygon:
+            polygons = row.geometry
+            for polygon in polygons.geoms:
+                x, y = np.array(polygon.exterior.xy)
+                fig.add_trace(go.Scattermap(
+                    lon=x,
+                    lat=y,
+                    mode='lines',
+                    line=dict(width=2, color='blue'),
+                    name=str(row.get('name', 'Polygon'))
+                ))
+                polygondb+=1
+        else:
+            x, y = np.array(row.geometry.exterior.xy)
+            fig.add_trace(go.Scattermap(
+                lon=x,
+                lat=y,
+                mode='lines',
+                line=dict(width=2, color='blue'),
+                name=str(row.get('name', 'Polygon'))
+            ))
+            polygondb+=1
 
     fig.update_layout(
         mapbox_style="carto-positron",
@@ -72,13 +87,17 @@ def showGraph_plotly():
         mapbox_center={"lat": gdf.geometry.centroid.y.mean(), "lon": gdf.geometry.centroid.x.mean()},
         margin={"r": 0, "t": 0, "l": 0, "b": 0}
     )
-
+    print("poligonok darabszáma: ", polygondb)
     fig.show()
 
 
 
+start = time.time()
 #shapefile_path = "asd.shp"
-shapefile_path = "shapefiles/alaska/trees.shp"
+#shapefile_path = "shapefiles/alaska/trees.shp"
+#shapefile_path = "shapefiles/andorra/gadm41_AND_1.shp"
+#shapefile_path = "shapefiles/hungary/gadm41_HUN_2.shp"
+shapefile_path = "shapefiles/bahamas/gadm41_BHS_1.shp"
 gdf = process_shapefile(shapefile_path)
 graph = nx.Graph()
 max_distance_for_graph = 10
@@ -86,9 +105,11 @@ max_distance_for_graph = 10
 
 
 
-#createGraph()
+createGraph()
 #showGraph_plotter()
 showGraph_plotly()
 print(graph)
 
+end = time.time()
+print(f"Futási idő: {end - start:.3f} másodperc")
 
